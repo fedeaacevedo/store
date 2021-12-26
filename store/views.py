86b5b5cc3b635c43c2d03b9467f9b420d49800c0
Federico <feeedeacevedo@gmail.com>
2021-12-26 11:26:05 -0300

    first commit

---
+++ store/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print(username)
+        print(password)
+
+    return render(request, 'usuarios/login.html', {
+
+    })    
